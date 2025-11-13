a93e7f7fa4cb04ca5aac4cd758f064fb31680aec
Marco Corradini <marco.corradini@itsincom.it>
2025-11-13 19:17:36 +0100

    Allined all pages to same css (Home - PROFILE)

--- Taskly/src/app/app.routes.ts
+++ Taskly/src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Home } from './home/home';
+import { Profile } from './profile/profile';
 
 export const routes: Routes = [
     {
         path: '',
         component: Home,
         title: 'Home page',
+    },
+    {
+        path: 'profile',
+        component: Profile,
+        title: 'Profile page',
     },
     {
         path: 'login',
