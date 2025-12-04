da8223807c64fbf886039a379a71e16c7cd983fc
Rabbi <rabbi.hossain@itsincom.it>
2025-12-04 11:55:37 +0100

    feat: refactor HeroCard component to use input properties for dynamic content

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../../components/sidebar/sidebar";
+import { HeroCard } from '../../components/hero-card/hero-card';
 
 @Component({
   selector: 'app-profile',
-  imports: [Sidebar],
+  imports: [Sidebar, HeroCard],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
 export class Profile {
-
+  TODO() {
+    
+  }
 }
